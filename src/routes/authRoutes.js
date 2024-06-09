7bf9d91a553d468d417e3db1a7d7dadd7d330916
RhuanMB <rhuanmb@gmail.com>
2024-06-09 18:50:04 -0300

    nova adição ao swagger

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,57 @@ import autenticado from "../middlewares/autenticado.js";
 
 const routes = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Autenticação
+ *   description: Gerenciamento de autenticação de usuários
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Faz login de um usuário
+ *     tags: [Autenticação]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               usuario:
+ *                 type: string
+ *                 example: "johndoe"
+ *               senha:
+ *                 type: string
+ *                 example: "SenhaForte123"
+ *     responses:
+ *       200:
+ *         description: Login realizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: Token JWT gerado
+ *                   example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+ *       401:
+ *         description: Falha na autenticação
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensagem de erro
+ *                   example: "Senha invalida"
+ */
+
 routes.post("/auth/login", AuthController.login);
 
 routes.use(autenticado);
